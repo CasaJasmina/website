a030c961709af957e637a303dcccf4e353fe343a
lorenzo <l.romagnoli@arduino.cc>
2016-04-05 16:00:07 +0200

    added new gitcommit plugin

---
+++ wp-content/plugins/gitCommit/gitCommit.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Plugin Name: gitCommit
+ * Plugin URI: http://cj_basic.com/widget
+ * Description: Open new issue on github
+ * Version: 0.1
+ * Author: Lorenzo Romagnoli
+ * Author URI: casajasmina.arduino.cc
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+ /** Step 2 (from text above). */
+ add_action( 'admin_menu', 'my_plugin_menu' );
+
+ /** Step 1. */
+ function my_plugin_menu() {
+ 	add_options_page( 'My Plugin Options', 'My Plugin', 'manage_options', 'my-unique-identifier', 'my_plugin_options' );
+ }
+
+ /** Step 3. */
+ function my_plugin_options() {
+ 	if ( !current_user_can( 'manage_options' ) )  {
+ 		wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
+ 	}
+ 	echo '<div class="wrap">';
+ 	echo '<p>Here is where the form would go if I actually had options.</p>';
+ 	echo '</div>';
+ }
+
+
+
+?>
