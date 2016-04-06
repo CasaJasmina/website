8e364fe14b7a07e4ee93a7c095457ce78ad2155b
tommasoltrz <tommasoltrz@gmail.com>
2016-04-06 12:05:56 +0200

    added basic template

---
+++ wp-content/themes/Arduino/page-git-commit.php
@@ -0,0 +1,83 @@
+<?php
+/**
+* The template for displaying all projects
+*
+* This is the template that displays all pages by default.
+* Please note that this is the WordPress construct of pages and that other
+* 'pages' on your WordPress site will use a different template.
+*
+* @package WordPress
+* @subpackage Casa_Jasmina
+* @since Casa Jasmina 1.0
+*/
+
+get_header(); ?>
+
+<div class="header-git-commit">
+</div>
+
+<div class="row text-intro">
+<p id="text-intro"> "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+</p>
+</div>
+
+<div class="row "><div class="large-12 columns large-centered text-questions-header">WE WOULD LOVE TO HAVE YOUR INPUT ON ONE OF THOSE TOPICS</div></div>
+
+
+  <div class="row git-commit-icons">
+    <div class="large-12 columns large-centered">
+      <div class="row">
+        <div class="large-3 small-3 text-center columns"> <img src='<?php echo bloginfo('template_directory') ?>/images/git-commit/Icon1.svg' /></div>
+          <div class="large-3 small-3 text-center columns"><img src='<?php echo bloginfo('template_directory') ?>/images/git-commit/Icon2.svg' /> </div>
+            <div class="large-3 small-3 text-center columns"><img src='<?php echo bloginfo('template_directory') ?>/images/git-commit/Icon3.svg' /></div>
+              <div class="large-3 small-3 text-center columns"> <img src='<?php echo bloginfo('template_directory') ?>/images/git-commit/Icon4.svg' /></div>
+            </div>
+          </div>
+        </div>
+
+<div class="container-footer">
+<div class="row git-commit-footer">
+  <div class="large-12 columns large-centered">
+    <div class="row">
+      <div class="large-3 small-3 text-center radius panel left columns"> PRIVACY</div>
+        <div class="large-3 small-3 text-center radius panel left columns"> SOCIAL INTERACTION </div>
+          <div class="large-3 small-3 text-center radius panel left columns"> OPEN DESIGN</div>
+            <div class="large-3 small-3 text-center radius panel left columns"> FAMILIAR OBJECTS</div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+<hr>
+
+<div class="row">
+  <divclass="large-12 columns">
+  <div class="background-text yellow">
+<h1>DATA AND PRIVACY</h1></br>
+<p>IS THERE ANITHYNG ABOUT </p></br>
+<p>YOUR LIFE YOU WOULD</p></br>
+<p>NEVER SHARE ONLINE?</p>
+  </div></div>
+
+  <div class="row">
+      <p id="text-questions"> In the house of the future would you share the information about your everyday activities
+                          and habits with other people? Are you willing to share the conversations you have with your
+                          relatives, flatmates, friends? The times you use an object or piece of  furniture? Your sleeping
+                          behaviours? The food you eat? The tv series you watch?
+      </p>
+  </div>
+
+
+<script>
+/*
+$(document).ready(function(){
+  $(window).scroll(function() {
+      if ($(this).scrollTop() > 200) {
+          $(".git-commit-footer").addClass("sticky");
+      } else if ($(this).scrollTop() < 200) {
+          $(".git-commit-footer").removeClass("sticky");
+      }
+  });
+});
+*/
+</script>
